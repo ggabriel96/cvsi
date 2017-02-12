480ddc0abb9a75765c33e3042631b1fc7a000890
Gabriel Galli <g7.galli96@gmail.com>
2017-02-12 18:40:17 -0200

    Added BroadcastReceiver for connectivity changes; bug fixes

--- app/src/main/java/io/github/ggabriel96/cvsi/android/activity/Home.java
+++ app/src/main/java/io/github/ggabriel96/cvsi/android/activity/Home.java
@@ -6,19 +6,19 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import io.github.ggabriel96.cvsi.android.R;
+import io.github.ggabriel96.cvsi.android.background.NetworkListener;
 
 public class Home extends AppCompatActivity {
 
+  protected static final FirebaseAuth auth = FirebaseAuth.getInstance();
+  protected static final NetworkListener networkListener = new NetworkListener();
   private static final String TAG = "Home";
-
-  protected static FirebaseAuth auth;
-
+  private static final int LOGIN_REQUEST = 1;
   private FirebaseUser user;
   private FirebaseAuth.AuthStateListener authListener;
 
@@ -26,22 +26,20 @@ public class Home extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    Home.auth = FirebaseAuth.getInstance();
+    Home.networkListener.register(this);
+
     this.authListener = new FirebaseAuth.AuthStateListener() {
       @Override
       public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
         Home.this.user = firebaseAuth.getCurrentUser();
-        if (user != null) {
+        if (Home.this.user != null) {
           // User is signed in
           Log.d(TAG, "onAuthStateChanged:signed_in: " + Home.this.user.getEmail() + "(" + Home.this.user.getUid() + ")");
           Home.this.init();
         } else {
           // User is signed out
-          /**
-           * @TODO see Intent options for when pressing back button
-           */
           Log.d(TAG, "onAuthStateChanged:signed_out");
-          Home.this.startActivity(new Intent(Home.this, Login.class));
+          Home.this.startActivityForResult(new Intent(Home.this, Login.class), Home.LOGIN_REQUEST);
         }
       }
     };
@@ -59,9 +57,24 @@ public class Home extends AppCompatActivity {
     if (this.authListener != null) Home.auth.removeAuthStateListener(this.authListener);
   }
 
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    if (Home.networkListener != null) networkListener.unregister(this);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == Home.LOGIN_REQUEST) {
+      if (resultCode != Login.RESULT_OK) this.finish();
+    } else {
+      super.onActivityResult(requestCode, resultCode, data);
+    }
+  }
+
   private void init() {
     this.setContentView(R.layout.activity_home);
-    ((Button) this.findViewById(R.id.logout)).setOnClickListener(new View.OnClickListener() {
+    this.findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
         Home.auth.signOut();
--- app/src/main/java/io/github/ggabriel96/cvsi/android/activity/SignUp.java
+++ app/src/main/java/io/github/ggabriel96/cvsi/android/activity/SignUp.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,51 +21,81 @@ public class SignUp extends AppCompatActivity {
 
   private static final String TAG = "SignUp";
 
+  private TextView emailView;
+  private TextView passwordView;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_sign_up);
+    this.emailView = (TextView) this.findViewById(R.id.email);
+    this.passwordView = (TextView) this.findViewById(R.id.password);
 
-    final TextView email = (TextView) this.findViewById(R.id.email);
-    final TextView password = (TextView) this.findViewById(R.id.password);
-    ((Button) this.findViewById(R.id.signup)).setOnClickListener(new View.OnClickListener() {
+    this.findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        /**
-         * @TODO add loading animation
-         */
-        email.setEnabled(Boolean.FALSE);
-        password.setEnabled(Boolean.FALSE);
-        Home.auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-          .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-              Log.d(TAG, "createUserWithEmail:onComplete:isSuccessful: " + task.isSuccessful());
+        if (Home.networkListener.isOnline()) {
+          /**
+           * @TODO add loading animation
+           */
+          SignUp.this.disableFields();
+          String email = emailView.getText().toString();
+          String password = passwordView.getText().toString();
+          if (email.isEmpty()) {
+            Toast.makeText(SignUp.this, R.string.email_empty, Toast.LENGTH_SHORT).show();
+            SignUp.this.enableFields();
+          } else if (password.isEmpty()) {
+            Toast.makeText(SignUp.this, R.string.password_weak, Toast.LENGTH_SHORT).show();
+            SignUp.this.enableFields();
+          } else {
+            Home.auth.createUserWithEmailAndPassword(email, password)
+              .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                  Log.d(TAG, "createUserWithEmail:onComplete:isSuccessful: " + task.isSuccessful());
 
-              // If sign in fails, display a message to the user. If sign in succeeds
-              // the auth state listener will be notified and logic to handle the
-              // signed in user can be handled in the listener.
-              if (task.isSuccessful()) {
-                SignUp.this.finish();
-              } else {
-                Exception authException = task.getException();
-                Log.w(TAG, "createUserWithEmail:failed", authException);
-                email.setEnabled(Boolean.TRUE);
-                password.setEnabled(Boolean.TRUE);
-                if (authException instanceof FirebaseAuthInvalidCredentialsException) {
-                  Toast.makeText(SignUp.this, R.string.email_invalid, Toast.LENGTH_SHORT).show();
-                } else if (authException instanceof FirebaseAuthUserCollisionException) {
-                  Toast.makeText(SignUp.this, R.string.email_exists, Toast.LENGTH_SHORT).show();
-                } else if (authException instanceof FirebaseException) {
-                  /**
-                   * @TODO FirebaseAuthWeakPasswordException not being thrown?
-                   */
-                  Toast.makeText(SignUp.this, R.string.password_weak, Toast.LENGTH_SHORT).show();
+                  // If sign in fails, display a message to the user. If sign in succeeds
+                  // the auth state listener will be notified and logic to handle the
+                  // signed in user can be handled in the listener.
+                  if (task.isSuccessful()) {
+                    SignUp.this.finishResultingOk();
+                  } else {
+                    Exception authException = task.getException();
+                    Log.w(TAG, "createUserWithEmail:failed", authException);
+                    SignUp.this.enableFields();
+                    if (authException instanceof FirebaseAuthInvalidCredentialsException) {
+                      Toast.makeText(SignUp.this, R.string.email_invalid, Toast.LENGTH_SHORT).show();
+                    } else if (authException instanceof FirebaseAuthUserCollisionException) {
+                      Toast.makeText(SignUp.this, R.string.email_exists, Toast.LENGTH_SHORT).show();
+                    } else if (authException instanceof FirebaseException) {
+                      /**
+                       * @TODO FirebaseAuthWeakPasswordException not being thrown?
+                       */
+                      Toast.makeText(SignUp.this, R.string.password_weak, Toast.LENGTH_SHORT).show();
+                    }
+                  }
                 }
-              }
-            }
-          });
+              });
+          }
+        } else {
+          Toast.makeText(SignUp.this, R.string.disconnected, Toast.LENGTH_SHORT).show();
+        }
       }
     });
+  }
+
+  private void finishResultingOk() {
+    this.setResult(SignUp.RESULT_OK);
+    this.finish();
+  }
+
+  private void enableFields() {
+    this.emailView.setEnabled(Boolean.TRUE);
+    this.passwordView.setEnabled(Boolean.TRUE);
+  }
+
+  private void disableFields() {
+    this.emailView.setEnabled(Boolean.FALSE);
+    this.passwordView.setEnabled(Boolean.FALSE);
   }
 }
