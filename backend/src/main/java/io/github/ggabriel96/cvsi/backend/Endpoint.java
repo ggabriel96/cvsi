011adf426e36f8bc4f4ec52e3a2668357b3c3601
Gabriel Galli <g7.galli96@gmail.com>
2017-02-22 16:46:03 -0300

    Just a few tests

--- backend/src/main/java/io/github/ggabriel96/cvsi/backend/Endpoint.java
+++ backend/src/main/java/io/github/ggabriel96/cvsi/backend/Endpoint.java
@@ -7,13 +7,36 @@
 package io.github.ggabriel96.cvsi.backend;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class Endpoint extends HttpServlet {
+
+  private static final Logger LOG = Logger.getLogger("io.github.ggabriel96.cvsi.backend.Endpoint");
+
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
     throws IOException {
+//    FirebaseOptions firebaseOptions = new FirebaseOptions.Builder()
+//      .setCredential(FirebaseCredentials.applicationDefault())
+//      .setDatabaseUrl("https://cvsi-backend.firebaseio.com/")
+//      .build();
+//
+//
+//    try {
+//      FirebaseApp.initializeApp(firebaseOptions);
+//    } catch (Exception error) {
+//      Endpoint.LOG.info("Already exists...");
+//    }
+//
+//    Storage storage = StorageOptions.getDefaultInstance().getService();
+//    Blob image = storage.get("cvsi-backend.appspot.com", "images/EnknEWerHud3wy3xORvstSbQnt52/98974275");
+//
+//    resp.setContentType(image.getContentType());
+//    resp.getOutputStream().write(image.getContent());
     String name = req.getParameter("name");
     resp.setContentType("text/plain");
     if (name != null) {
